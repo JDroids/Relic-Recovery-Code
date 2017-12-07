eddb35629a971f57ee691ea946b776d0c0110421
dansman805 <dansman805@gmail.com>
2017-12-06 21:50:31 -0500

    Added Hello World Program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+/**
+ * Created by dansm on 12/6/2017.
+ */
+
+@TeleOp
+
+public class HelloWorld extends LinearOpMode{
+    @Override
+
+    public void runOpMode() throws InterruptedException{
+
+        telemetry.addData("Message", "Hello World!");
+        telemetry.update();
+
+        //Code to run after init is pressed
+        waitForStart();
+        int i = 0;
+        while(opModeIsActive()){
+            telemetry.addData("Approximate Amount of Seconds Passed:", i);
+            telemetry.update();
+            Thread.sleep(1000);
+            i+=1;
+        }
+        //Code to run after play is pressed
+    }
+}
